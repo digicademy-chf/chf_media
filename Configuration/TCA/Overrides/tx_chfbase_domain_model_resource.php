8f5f3fc21a7bddc8af2d5c6a48f02009d6d9b5e2
Jonatan Jalle Steller <jonatan.steller@adwmainz.de>
2024-03-23 18:58:02 +0100

    Add allFileGroups to AbstractResource

---
+++ Configuration/TCA/Overrides/tx_chfbase_domain_model_resource.php
@@ -0,0 +1,47 @@
+<?php
+declare(strict_types=1);
+
+# This file is part of the extension CHF Media for TYPO3.
+#
+# For the full copyright and license information, please read the
+# LICENSE.txt file that was distributed with this source code.
+
+
+defined('TYPO3') or die();
+
+/**
+ * AbstractResource and its properties
+ * 
+ * Extension of a database table and its editing interface in the
+ * TYPO3 backend. This also serves as the basis for the Extbase
+ * domain model. For more information on TCA and its options see
+ * https://docs.typo3.org/m/typo3/reference-tca/main/en-us/.
+ */
+
+// Add column 'allFileGroups'
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns('tx_chfbase_domain_model_resource',
+    [
+        'allFileGroups' => [
+            'exclude' => true,
+            'l10n_mode' => 'exclude',
+            'label' => 'LLL:EXT:chf_media/Resources/Private/Language/locallang.xlf:object.abstractResource.allFileGroups',
+            'description' => 'LLL:EXT:chf_media/Resources/Private/Language/locallang.xlf:object.abstractResource.allFileGroups.description',
+            'config' => [
+                'type' => 'inline',
+                'foreign_table' => 'tx_chfmedia_domain_model_file_group',
+                'foreign_field' => 'parentResource',
+                'foreign_sortby' => 'sorting',
+                'appearance' => [
+                    'collapseAll' => true,
+                    'expandSingle' => true,
+                    'newRecordLinkAddTitle' => true,
+                    'levelLinksPosition' => 'top',
+                    'useSortable' => false,
+                    'showPossibleLocalizationRecords' => true,
+                    'showAllLocalizationLink' => true,
+                    'showSynchronizationLink' => true,
+                ],
+            ],
+        ],
+    ]
+);
