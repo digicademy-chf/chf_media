e412b407a6cc4d61a083ddff047f640f8502d64c
Jonatan Jalle Steller <jonatan.steller@adwmainz.de>
2024-11-22 18:23:12 +0100

    First set of frontend fixes

--- Classes/Domain/Repository/FileGroupRepository.php
+++ Classes/Domain/Repository/FileGroupRepository.php
@@ -9,6 +9,7 @@ declare(strict_types=1);
 
 namespace Digicademy\CHFMedia\Domain\Repository;
 
+use Digicademy\CHFBase\Domain\Repository\Traits\StoragePageAgnosticTrait;
 use Digicademy\CHFMedia\Domain\Model\FileGroup;
 use TYPO3\CMS\Extbase\Persistence\QueryInterface;
 use TYPO3\CMS\Extbase\Persistence\Repository;
@@ -22,6 +23,8 @@ defined('TYPO3') or die();
  */
 class FileGroupRepository extends Repository
 {
+    use StoragePageAgnosticTrait;
+
     protected $defaultOrderings = [
         'sorting'     => QueryInterface::ORDER_ASCENDING,
         'isHighlight' => QueryInterface::ORDER_ASCENDING,
