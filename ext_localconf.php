1f903b9990ad63c4a81b0597bcdb14cc46dd5978
Jonatan Jalle Steller <jonatan.steller@adwmainz.de>
2024-12-18 00:07:00 +0100

    Use sRGB2014 colour profile in images

--- ext_localconf.php
+++ ext_localconf.php
@@ -12,6 +12,16 @@ use TYPO3\CMS\Extbase\Utility\ExtensionUtility;
 
 defined('TYPO3') or die();
 
+// Set image options (remove blur, set sRGB, improve quality)
+$GLOBALS['TYPO3_CONF_VARS']['GFX']['processor_effects'] = 0;
+$GLOBALS['TYPO3_CONF_VARS']['GFX']['processor_stripColorProfileParameters'] = [
+    '-profile',
+    '../vendor/digicademy/chf_media/Resources/Private/Icc/sRGB2014.icc',
+];
+$GLOBALS['TYPO3_CONF_VARS']['GFX']['avif_quality'] = 90;
+$GLOBALS['TYPO3_CONF_VARS']['GFX']['jpg_quality'] = 90;
+$GLOBALS['TYPO3_CONF_VARS']['GFX']['webp_quality'] = 90;
+
 // Register 'Gallery' content element
 ExtensionUtility::configurePlugin(
     'CHFMedia',
